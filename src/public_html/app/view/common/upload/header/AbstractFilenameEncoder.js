c545089640f39cd6e7c888b48d8a6fbbdc91c0da
fikipollo <rafahd87@gmail.com>
2016-10-13 17:26:27 +0200

    Release v0.4.3.2

---
+++ src/public_html/app/view/common/upload/header/AbstractFilenameEncoder.js
@@ -0,0 +1,1 @@
+Ext.define("Ext.upload.header.AbstractFilenameEncoder",{config:{},type:"generic",contructor:function(config){this.initConfig(config)},encode:function(filename){},getType:function(){return this.type}});
